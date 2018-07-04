a22ceaa14a9b0c387a3c13268626300704b1bd08
Martin van Houte <mvhoute@gmail.com>
2018-07-04 21:16:55 +0200

    Added editorconfig + translated Share this post

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -9,7 +9,7 @@ function Share({ title, url }) {
   const text = encodeURI(title);
   return (
     <Wrapper>
-      <H2>Share This Post</H2>
+      <H2>Deel dit artikel</H2>
       <ShareButton href={`https://www.facebook.com/sharer/sharer.php?u=${encodedURL}`} target="_blank" aria-label="Share on Facebook">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path></svg>
       </ShareButton>
